98994d10136159183e6a3b4711022cc3098bf72e
DanielaCazac <danycazac97@yahoo.com>
2018-04-23 23:17:25 +0300

    lab4

---
+++ lab4/Mail/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Mail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_mailFrom = new System.Windows.Forms.TextBox();
+            this.textBox_passwd = new System.Windows.Forms.TextBox();
+            this.textBox_mailTo = new System.Windows.Forms.TextBox();
+            this.textBox_Subject = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox_mailText = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_read = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxCC = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 481);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_mailFrom
+            // 
+            this.textBox_mailFrom.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.textBox_mailFrom.Location = new System.Drawing.Point(204, 44);
+            this.textBox_mailFrom.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_mailFrom.Name = "textBox_mailFrom";
+            this.textBox_mailFrom.Size = new System.Drawing.Size(271, 22);
+            this.textBox_mailFrom.TabIndex = 1;
+            // 
+            // textBox_passwd
+            // 
+            this.textBox_passwd.Location = new System.Drawing.Point(204, 73);
+            this.textBox_passwd.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_passwd.Name = "textBox_passwd";
+            this.textBox_passwd.Size = new System.Drawing.Size(271, 22);
+            this.textBox_passwd.TabIndex = 2;
+            this.textBox_passwd.UseSystemPasswordChar = true;
+            // 
+            // textBox_mailTo
+            // 
+            this.textBox_mailTo.Location = new System.Drawing.Point(204, 158);
+            this.textBox_mailTo.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_mailTo.Name = "textBox_mailTo";
+            this.textBox_mailTo.Size = new System.Drawing.Size(271, 22);
+            this.textBox_mailTo.TabIndex = 3;
+            // 
+            // textBox_Subject
+            // 
+            this.textBox_Subject.Location = new System.Drawing.Point(204, 231);
+            this.textBox_Subject.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Subject.Name = "textBox_Subject";
+            this.textBox_Subject.Size = new System.Drawing.Size(271, 22);
+            this.textBox_Subject.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Email From :";
+            // 
+            // richTextBox_mailText
+            // 
+            this.richTextBox_mailText.Location = new System.Drawing.Point(204, 280);
+            this.richTextBox_mailText.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox_mailText.Name = "richTextBox_mailText";
+            this.richTextBox_mailText.Size = new System.Drawing.Size(271, 99);
+            this.richTextBox_mailText.TabIndex = 7;
+            this.richTextBox_mailText.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Password :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 158);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Email to :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 231);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Subject :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 280);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Email text :";
+            // 
+            // button_read
+            // 
+            this.button_read.Location = new System.Drawing.Point(376, 481);
+            this.button_read.Margin = new System.Windows.Forms.Padding(4);
+            this.button_read.Name = "button_read";
+            this.button_read.Size = new System.Drawing.Size(100, 28);
+            this.button_read.TabIndex = 13;
+            this.button_read.Text = "Read";
+            this.button_read.UseVisualStyleBackColor = true;
+            this.button_read.Click += new System.EventHandler(this.button_read_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 192);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Email CC :";
+            // 
+            // textBoxCC
+            // 
+            this.textBoxCC.Location = new System.Drawing.Point(204, 192);
+            this.textBoxCC.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxCC.Name = "textBoxCC";
+            this.textBoxCC.Size = new System.Drawing.Size(271, 22);
+            this.textBoxCC.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 524);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxCC);
+            this.Controls.Add(this.button_read);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox_mailText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Subject);
+            this.Controls.Add(this.textBox_mailTo);
+            this.Controls.Add(this.textBox_passwd);
+            this.Controls.Add(this.textBox_mailFrom);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_mailFrom;
+        private System.Windows.Forms.TextBox textBox_passwd;
+        private System.Windows.Forms.TextBox textBox_mailTo;
+        private System.Windows.Forms.TextBox textBox_Subject;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox_mailText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_read;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxCC;
+    }
+}
+
---
+++ lab4/Mail/ReadMail.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Mail
+{
+    partial class ReadMail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.textBox_passwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_retrieve = new System.Windows.Forms.Button();
+            this.listViewMails = new System.Windows.Forms.ListView();
+            this.from = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.subject = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.mesage = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password :";
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(387, 22);
+            this.textBox_email.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(244, 22);
+            this.textBox_email.TabIndex = 2;
+            // 
+            // textBox_passwd
+            // 
+            this.textBox_passwd.Location = new System.Drawing.Point(387, 58);
+            this.textBox_passwd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_passwd.Name = "textBox_passwd";
+            this.textBox_passwd.Size = new System.Drawing.Size(244, 22);
+            this.textBox_passwd.TabIndex = 3;
+            this.textBox_passwd.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(383, 213);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "messages";
+            // 
+            // button_retrieve
+            // 
+            this.button_retrieve.Location = new System.Drawing.Point(16, 382);
+            this.button_retrieve.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_retrieve.Name = "button_retrieve";
+            this.button_retrieve.Size = new System.Drawing.Size(247, 28);
+            this.button_retrieve.TabIndex = 10;
+            this.button_retrieve.Text = "Retrieve";
+            this.button_retrieve.UseVisualStyleBackColor = true;
+            this.button_retrieve.Click += new System.EventHandler(this.button_retrieve_Click);
+            // 
+            // listViewMails
+            // 
+            this.listViewMails.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.from,
+            this.date,
+            this.subject,
+            this.mesage});
+            this.listViewMails.Location = new System.Drawing.Point(16, 199);
+            this.listViewMails.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listViewMails.Name = "listViewMails";
+            this.listViewMails.Size = new System.Drawing.Size(852, 157);
+            this.listViewMails.TabIndex = 11;
+            this.listViewMails.UseCompatibleStateImageBehavior = false;
+            this.listViewMails.View = System.Windows.Forms.View.Details;
+            // 
+            // from
+            // 
+            this.from.Text = "from";
+            this.from.Width = 179;
+            // 
+            // date
+            // 
+            this.date.Text = "date";
+            // 
+            // subject
+            // 
+            this.subject.Text = "subject";
+            this.subject.Width = 130;
+            // 
+            // mesage
+            // 
+            this.mesage.Text = "mesage";
+            this.mesage.Width = 210;
+            // 
+            // ReadMail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 425);
+            this.Controls.Add(this.listViewMails);
+            this.Controls.Add(this.button_retrieve);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_passwd);
+            this.Controls.Add(this.textBox_email);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ReadMail";
+            this.Text = "ReadMail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.TextBox textBox_passwd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_retrieve;
+        private System.Windows.Forms.ListView listViewMails;
+        private System.Windows.Forms.ColumnHeader subject;
+        private System.Windows.Forms.ColumnHeader mesage;
+        public System.Windows.Forms.ColumnHeader from;
+        private System.Windows.Forms.ColumnHeader date;
+    }
+}
